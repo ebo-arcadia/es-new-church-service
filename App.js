2a712678cd17d75fc827dca802a451943120241c
Ebo Arcadia <yiboli.coding@gmail.com>
2023-06-03 11:18:51 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Your book list" />
+          <Button title="Add a book" />
+        </View>
+        <View style={styles.booksContainer}>
+          <Text>List of books...</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 100,
+    paddingHorizontal: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 22,
+    borderBottomWidth: 2,
+    borderBottomColor: "blue",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "green",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  booksContainer: {
+    backgroundColor: "red",
+  },
+  flexContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
